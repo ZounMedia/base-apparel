2de3e3351bddb3b269fb97e12b19726b0333e24e
Romaric Zounlome <rzounlom@gmail.com>
2023-09-29 09:03:31 -0400

    complete tablet styles

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ let input = document.querySelector("input");
 const submitBtn = document.querySelector("button");
 let errorText = document.querySelector("span.error-text");
 const errorIcon = document.querySelector("img.error");
+const form = document.querySelector("form");
 
 //add click event listener to button
 submitBtn.addEventListener("click", (event) => {
@@ -18,6 +19,7 @@ submitBtn.addEventListener("click", (event) => {
   ) {
     errorText.classList.add("active");
     errorIcon.classList.add("active");
+    form.classList.add("active");
     //prevent form from submitting
     event.preventDefault();
     return;
